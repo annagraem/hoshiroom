0fce857dfee0d5cfaaedc6fd3a423d01e16b67eb
annafougeras <anna.fougeras@gmail.com>
2023-04-29 15:41:14 +0200

    Create base interactable class

---
+++ scenes/interactable/interactable.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+@export var dialogue_resource: DialogueResource
+@export var dialogue_start: String
+
+func interact():
+    DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
