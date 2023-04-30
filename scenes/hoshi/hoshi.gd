78aa3d09439b68656a47e0e1e19828ea9dcd0a19
Anna Fougeras <anna.fougeras@gmail.com>
2023-04-30 15:16:10 +0200

    Move character in the room

---
+++ scenes/hoshi/hoshi.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+@export var speed := 50.0
+@onready var animations = $AnimationPlayer
+
+func update_animations() -> void:
+    
+    if velocity.length() == 0:
+        if (animations.is_playing()):
+            animations.stop()
+    else:
+        var direction = "down"
+        if velocity.x < 0:
+            direction = "left"
+        elif velocity.x > 0:
+            direction = "right"
+        elif velocity.y < 0:
+            direction = "up" 
+        animations.play("walk_" + direction)
+    
+
+func _physics_process(delta: float) -> void:
+    var input_direction = Input.get_vector("left", "right", "up", "down").normalized()
+    velocity = input_direction * speed
+    update_animations()
+    move_and_slide()
